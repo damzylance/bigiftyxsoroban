70c29e58dbd29b1d5a2e216f193dece33614f192
damzylance <32623753+damzylance@users.noreply.github.com>
2024-10-15 00:47:08 +0100

    implement isAllowed

--- app/utils/hooks/useConnectWallet.tsx
+++ app/utils/hooks/useConnectWallet.tsx
@@ -1,4 +1,9 @@
-import { isConnected, getAddress, requestAccess } from "@stellar/freighter-api";
+import {
+	isConnected,
+	getAddress,
+	requestAccess,
+	isAllowed,
+} from "@stellar/freighter-api";
 import { useWallet } from "@/app/context/WalletContext";
 
 export const useConnectWallet = () => {
@@ -6,11 +11,20 @@ export const useConnectWallet = () => {
 	const connectWallet = async () => {
 		try {
 			if (await isConnected()) {
-				console.log(await isConnected());
-				const publicKey = await getAddress();
-				console.log(publicKey);
-				setWalletAddress(publicKey.address);
-				console.log("Connected with public key:", publicKey);
+				if (await isAllowed()) {
+					const publicKey = await getAddress();
+					console.log(publicKey);
+					setWalletAddress(publicKey.address);
+					console.log("Connected with public key:", publicKey);
+				} else {
+					const isAppAllowed = await isAllowed();
+					if (isAppAllowed.isAllowed) {
+						const publicKey = await getAddress();
+						console.log(publicKey);
+						setWalletAddress(publicKey.address);
+						console.log("Connected with public key:", publicKey);
+					}
+				}
 			} else {
 				const accessObj = await requestAccess();
 
